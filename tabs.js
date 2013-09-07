1e91ceb66c4d64694d97bbe83a49c313aa7d097b
Brian Chu <bc@brianchu.com>
2013-09-07 02:02:38 -0400

    Basic tabs insertion code

--- tabs.js
+++ tabs.js
@@ -1,9 +1,21 @@
 // Background script:
 // http://developer.chrome.com/extensions/tabs.html#apiReference
 
+var currentTab;
+var tabs;
 
-chrome.tabs.onCreated.addListener(function(Tab tab) {...}); 
-chrome.tabs.onUpdated.addListener(function(integer tabId, object changeInfo, Tab tab) {...}); 
-chrome.tabs.onMoved.addListener(function(integer tabId, object moveInfo) {...}); 
-chrome.tabs.onActivated.addListener(function(object activeInfo) {...}); 
-chrome.tabs.onRemoved.addListener(function(integer tabId, object removeInfo) {...}); 
+// Startup
+chrome.tabs.query({}, function(tabsIn) {
+  tabs = tabsIn;
+});
+
+chrome.tabs.onCreated.addListener(function(tab) {
+  tabs.splice(tab.index,0,tab);
+});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  var idx = tab.index;
+  tab[idx] = tab;
+});
+// chrome.tabs.onMoved.addListener(function(tabId, moveInfo) {}); 
+// chrome.tabs.onActivated.addListener(function(activeInfo) {...}); 
+// chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {...}); 
